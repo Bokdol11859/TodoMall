d691f0ea8bbbc83406015a8ac709a1be52ead13d
bokdol11859 <2019147551@yonsei.ac.kr>
2023-01-24 21:24:38 +0900

    [Build] Install react query devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import Layout from '../components/global/Layout';
 import { Provider } from 'react-redux';
 import { store } from '@src/common/redux/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Layout>
           <Component {...pageProps} />
         </Layout>
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </Provider>
   );
