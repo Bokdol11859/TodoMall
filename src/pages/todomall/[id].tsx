75bcd001d8bba2bdbb24cddc15af98a803d81209
bokdol11859 <2019147551@yonsei.ac.kr>
2023-02-07 00:38:58 +0900

    [Feat] Implement Product detail page information component

--- src/pages/todomall/[id].tsx
+++ src/pages/todomall/[id].tsx
@@ -7,6 +7,7 @@ import { getProductDetail } from '@src/common/api/fetcher';
 import Image from 'next/image';
 import COLOR from '@src/common/constants/Colors';
 import Tags from '@src/components/global/Tags';
+import { TagIcon } from '@src/common/constants/TagIcon';
 
 const Detail = () => {
   const { query } = useRouter();
@@ -22,6 +23,22 @@ const Detail = () => {
         <ProductTitle>{productQuery.data?.title}</ProductTitle>
         <Tags tags={productQuery.data?.informationTags} />
       </ProductHeader>
+      <ProductInformation>
+        <ProductPriceContainer>
+          <p>클래스 구매가</p>
+          <ProductPrice>{10000}원</ProductPrice>
+        </ProductPriceContainer>
+        <ProductTags>
+          {productQuery.data?.summarizedTags.map((tag: any) => (
+            <ProductTagWrapper>
+              <ProductTag>
+                <ProductTagIcon src={`/images/${TagIcon[tag]}.svg`} />
+              </ProductTag>
+              <ProductTagName>{tag}</ProductTagName>
+            </ProductTagWrapper>
+          ))}
+        </ProductTags>
+      </ProductInformation>
     </Container>
   );
 };
@@ -58,6 +75,76 @@ const ProductTitle = styled.h1`
   font-weight: 700;
   font-size: 1.5rem;
   line-height: 2rem;
+`;
+
+const ProductInformation = styled.div`
+  margin: 1.5rem 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 0 1rem;
+  gap: 1.5rem;
+`;
+
+const ProductPriceContainer = styled.div`
+  background-color: white;
+  border-radius: 1rem;
+  height: 4rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 1.5rem;
+  font-weight: 500;
+  font-size: 1rem;
+  line-height: 1.5rem;
+`;
+
+const ProductPrice = styled.p`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.5rem;
+  line-height: 2rem;
+  color: ${COLOR.PURPLE500};
+`;
+
+const ProductTags = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1.5rem;
+  padding: 0 1rem;
+`;
+
+const ProductTagWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const ProductTag = styled.div`
+  width: 3rem;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${COLOR.PURPLE50};
+  border-radius: 100%;
+`;
+
+const ProductTagIcon = styled.img`
+  width: 1.5rem;
+  height: 1.5rem;
+`;
+
+const ProductTagName = styled.p`
+  margin: 0;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 0.75rem;
+  line-height: 1.5rem;
+  color: ${COLOR.GRAY400};
 `;
 
 export default Detail;
